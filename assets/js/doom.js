c2a400d322fc0f2bbc30fbc8965cb88696701430
Sam Davenport <samueldavenport@att.net>
2021-11-06 11:51:50 -0500

    test new api call

--- assets/js/doom.js
+++ assets/js/doom.js
@@ -23,13 +23,13 @@ var displayDoomInfo = function(doom) {
 }
 
 var getDoomData = function() {
-    var apiUrl = "https://gateway.marvel.com:443/v1/public/characters/1009281?apikey=4d2e0c623d661843745081ec1b70a4bb";
+    var apiUrl = "https://gateway.marvel.com:443/v1/public/characters/1009281/images?limit=100&apikey=4d2e0c623d661843745081ec1b70a4bb";
     
     fetch(apiUrl).then(function(response) {
         if(response.ok) {
         response.json().then(function(data) {
             console.log(data); 
-            displayDoomInfo(data);          
+            //displayDoomInfo(data);          
         });
         } else {
             console.log("Try Again, Dummy");
