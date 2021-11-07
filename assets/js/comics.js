1939a9104e35c09b26b9ac4aeb38bcf4b7616169
Sam Davenport <samueldavenport@att.net>
2021-11-07 10:31:18 -0600

    test

--- assets/js/comics.js
+++ assets/js/comics.js
@@ -96,23 +96,26 @@ var displayDoomComics = function(comicData) {
          creatorListEl.classList = "creators-list";
 
          var creatorsArr = DoomComicsArr[issue].creators.items;
+         if (creatorsArr[a] === {}) {
+             var noneEl = document.createElement('p');
+             noneEl.classList = "creator-list-item";
+            noneEl.innerHTML = "Creators Unavailable";
+            modal.appendChild(noneEl);
+
+        } else {
         
          for (var a = 0; a < creatorsArr.length; a++) {
             var creatorsEl = document.createElement('li');
             var job = creatorsArr[a].role;
             var person = creatorsArr[a].name;
             var creators = "<strong>" + job + ": " + "</strong>" + person;
-            
-            if (creatorsArr[a] = {}) {
-                creatorsEl.innerHTML = "Creators Unavailable";
-            } else {
             creatorsEl.innerHTML = creators;
-            }
+            
         creatorsEl.classList = "creator-list-item";
         creatorListEl.appendChild(creatorsEl);
         modal.appendChild(creatorListEl);
         
-            
+         }    
          
          displayComicModal();
         }
